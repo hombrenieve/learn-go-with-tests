499060cfe42119e98e860239781c63590d8f23cf
Pablo Rebaque Diaz <hombrenieve@gmail.com>
2019-09-28 00:37:15 +0200

    Arrays and ranges chapter some more code

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -1,17 +1,26 @@
 package slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
+	numbers := []int{1, 2, 3, 4}
 
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4}
+	got := Sum(numbers)
+	want := 10
 
-		got := Sum(numbers)
-		want := 10
+	if want != got {
+		t.Errorf("expected %d but got %d, given %v", want, got, numbers)
+	}
+}
 
-		if want != got {
-			t.Errorf("expected %d but got %d, given %v", want, got, numbers)
-		}
-	})
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 4, 9})
+	want := []int{3, 13}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v when expecting %v", got, want)
+	}
 }
